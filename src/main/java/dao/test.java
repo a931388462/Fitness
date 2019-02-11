2e7178e2cbac17dde39148abab90f0fa7a3beef2
FanWeiMeng <931388462@qq.com>
2019-02-11 10:06:13 +0800

    文字表示

--- src/main/java/dao/test.java
+++ src/main/java/dao/test.java
@@ -15,14 +15,14 @@ public class test {
 	public static void main(String[] args) throws Exception {
 		ReadExecl poi = new ReadExecl();
 		// List<List<String>> list = poi.read("d:/aaa.xls");
-		List<List<String>> list = poi.read("C:\\Users\\MFW\\Desktop\\aa.xlsx");
+		List<List<String>> list = poi.read("aa.xlsx");
 		if (list != null) {
 			for (int i = 0; i < list.size(); i++) {
-				System.out.print("第" + (i) + "行");
+				System.out.print("第" + (i) + "行\t");
 				List<String> cellList = list.get(i);
 				for (int j = 0; j < cellList.size(); j++) {
-					// System.out.print(" 第" + (j + 1) + "列值：");
-					System.out.print("    " + cellList.get(j));
+					System.out.print(" 第" + (j + 1) + "列值：");
+					System.out.print(cellList.get(j)+"\t");
 				}
 				System.out.println();
 			}
